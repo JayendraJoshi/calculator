aae8720897b58343899197ec6e777ec62ba003ce
jayendraJoshi <jayjoshi@outlook.de>
2023-02-12 12:46:26 +0100

    Create calculator layout & add operate function

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ return a*b;
 function divide(a,b){
 return a/b;
 }
+function operate(o,a,b){
+switch(o)
+{
+    case"+":
+      return  add(a,b);
+    case"-":
+      return  subtract(a,b);
+    case"*":
+      return  multiply(a,b);
+    case"/":
+      return  divide(a,b);
+}
+}
+
