6c4f2714ac9a7ce9c603c9b7038d8acbd17604c5
jayendra <jayendralal.joshi@gfk.com>
2025-02-18 13:34:50 +0100

    Add html,css and js files, add basic functions, styling and html
    add functions add,subtract,divide & multiplay
    
    add html boilerplate with buttons and display
    
    use css grid to place buttons in correct order, use flexbox to center calculator

---
+++ script.js
@@ -0,0 +1,16 @@
+let number1;
+let number2;
+let operator;
+
+function add(number1, number2){
+    return number1 + number2;
+}
+function subtract(number1, number2){
+    return number1 - number2;
+}
+function multiply(number1, number2){
+    return number1 * number2;
+}
+function divide(number1, number2){
+    return number1 / number2;
+}
