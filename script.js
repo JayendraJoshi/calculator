ebd38d53c39317c2960177ec6829ef0320bccd49
jayendra <jayendralal.joshi@gfk.com>
2025-02-19 10:51:26 +0100

    Refactor logic, improve modularity and seperation of methods, enable user to continue  calculating +,-,*,/ based on previous result

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let number1 = "";
 let number2 = "";
 let operator = "";
+let result = "";
+
 let isNumber1Set = false;
 let isNumber2Set = false;
 let isOperatorSet = false;
+let caluculateBasedOnResult = false;
 
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
@@ -26,49 +29,27 @@ function divide(number1, number2){
     return number1 / number2;
 }
 function operate(number1, number2, operator){
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
     switch(operator){
         case "+":
-            display.textContent = add(number1,number2);
+            result = add(number1,number2);
             break;
         case "-":
-            display.textContent = subtract(number1,number2);
+            result = subtract(number1,number2);
             break;
         case "*":
-            display.textContent = multiply(number1,number2);
+            result = multiply(number1,number2);
             break;
         case "/":
-            display.textContent = divide(number1,number2);
+            result = divide(number1,number2);
             break;
     }   
+    clear();
+    caluculateBasedOnResult = true;
+    updateDisplay();
 }
-numberButtons.forEach(button => {
-    button.addEventListener("click", (event) => {
-        if (!isNumber1Set){
-            number1 += event.target.textContent;
-            display.textContent = number1;
-        }else{
-            number2 += event.target.textContent;
-            display.textContent +=number2;
-        }
-    });
-});
-operatorButtons.forEach(button =>{
-    button.addEventListener("click", (event)=>{
-        if(operator!=event.target.textContent){
-            operator = event.target.textContent; 
-            display.textContent = number1 +" "+ operator;
-            isNumber1Set=true;
-            isOperatorSet=true;
-        }
-    })
-})
-equalsButton.addEventListener("click",(event)=>{
-    isNumber2Set = true;
-    if(isNumber1Set && isNumber2Set && isOperatorSet){
-        operate(number1,number2,operator);
-    }
-})
-clearButton.addEventListener("click",(event)=>{
+function clear(){
     display.textContent="0";
     number1="";
     number2="";
@@ -77,4 +58,65 @@ clearButton.addEventListener("click",(event)=>{
     isNumber1Set=false;
     isNumber2Set=false;
     isOperatorSet=false;
+    caluculateBasedOnResult =false;
+}
+function updateDisplay(){
+    if(result!=""){
+        display.textContent=result;
+    }else if(number1!=""&&operator==""&&number2==""){
+        display.textContent=number1;
+    }else if(number1!=""&&operator!=""&&number2==""){
+        display.textContent=number1 + " " + operator;
+    }else if(number1!=""&&operator!=""&&number2!=""){
+        display.textContent=number1 + " " + operator + number2;
+    }else{
+        display.textContent="0";
+    }
+}
+function handleNumberButtonsEvent(e){
+    console.log(isNumber1Set);
+    console.log(isNumber2Set);
+    console.log(isOperatorSet);
+    if(caluculateBasedOnResult){
+        number1 = result;
+        result = "";
+        updateDisplay();
+        caluculateBasedOnResult = false;
+        return;
+    }
+    if(!isNumber1Set){
+        number1 +=e.textContent;
+        updateDisplay();
+    }else if(isNumber1Set && !isNumber2Set){
+        number2 +=e.textContent;
+        updateDisplay();
+    }
+}
+function handleOperatorButtonsEvent(e){
+    console.log("handleOperatorButtonsEvent function reached!")
+    if(caluculateBasedOnResult){
+        number1 = result;
+        result = "";
+        caluculateBasedOnResult = false;
+    }
+    if(number1!=""){
+        operator = e.textContent;
+        console.log(operator);
+        isOperatorSet = true;
+        isNumber1Set = true;
+        updateDisplay();
+    }
+}
+numberButtons.forEach(button => {
+    button.addEventListener("click", (event) => handleNumberButtonsEvent(event.target));
+});
+operatorButtons.forEach(button =>{
+    button.addEventListener("click", (event)=> handleOperatorButtonsEvent(event.target));     
+});
+equalsButton.addEventListener("click",(event)=>{
+    if(isNumber1Set && isOperatorSet && number2!=""){
+        isNumber2Set = true;
+        operate(number1,number2,operator);
+    }
 })
+clearButton.addEventListener("click",clear);
