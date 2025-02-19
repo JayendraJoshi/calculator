e1f5cd987b9cee66a1fc1f56ec7bca4587bb22ea
jayendra <jayendralal.joshi@gfk.com>
2025-02-19 13:13:59 +0100

    Fix bugs,limit size of numberInput and add style
    allow user to properly calculate with previous result and use all functionality
    
    allow user to chain calculations without bugs
    
    limit number input to prevent overflowing
    
    style calculator to improve design

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let number1 = "";
 let number2 = "";
 let operator = "";
 let result = "";
+let savedResult = "";
 
 let isNumber1Set = false;
 let isNumber2Set = false;
@@ -29,6 +30,14 @@ function multiply(number1, number2){
 function divide(number1, number2){
     return number1 / number2;
 }
+function checkIfNumberIsTooLarge(number){
+    let numberAsString = number.toString();
+    if(numberAsString.length>=13){
+        return true;
+    }else{
+        return false;
+    }
+}
 function operate(number1, number2, operator){
     number1 = parseFloat(number1);
     number2 = parseFloat(number2);
@@ -39,44 +48,52 @@ function operate(number1, number2, operator){
         case "-":
             result = subtract(number1,number2);
             break;
-        case "*":
+        case "×":
             result = multiply(number1,number2);
             break;
-        case "/":
+        case "÷":
             result = divide(number1,number2);
             break;
     }   
+    if(checkIfNumberIsTooLarge(result)){
+        console.log("check reached");
+        result = NaN;
+    }
+    savedResult = result;
+    updateDisplay();
     clear();
     caluculateBasedOnResult = true;
-    updateDisplay();
 }
 function clear(){
-    display.textContent="0";
+    number1 = number1.toString();
     number1="";
+    number2 = number2.toString();
     number2="";
     operator="";
+    result="";
 
     isNumber1Set=false;
     isNumber2Set=false;
     isOperatorSet=false;
-    caluculateBasedOnResult =false;
+    updateDisplay();
 }
 function updateDisplay(){
-    if(result!=""){
-        display.textContent=result;
+    if(savedResult!=""){
+        display.textContent=savedResult;
     }else if(number1!=""&&operator==""&&number2==""){
         display.textContent=number1;
     }else if(number1!=""&&operator!=""&&number2==""){
-        display.textContent=number1 + " " + operator;
+        display.textContent=number1;
     }else if(number1!=""&&operator!=""&&number2!=""){
-        display.textContent=number1 + " " + operator + number2;
+        display.textContent=number2;
     }else{
         display.textContent="0";
     }
 }
 function handleCalculateBasedOnResult(){
-    number1 = result;
-        result = "";
+    savedResult = savedResult.toString();
+    number1 = savedResult;
+        savedResult = "";
         updateDisplay();
         caluculateBasedOnResult = false;
 }
@@ -87,29 +104,41 @@ function handleNumberButtonsEvent(e){
     if(caluculateBasedOnResult){
         if(e.textContent == 0){
             clear();
-            result = "";
-            caluculateBasedOnResult = false;
+            savedResult="";
             updateDisplay();
             return;
         }
         handleCalculateBasedOnResult();
     }
     if(!isNumber1Set){
+        if(checkIfNumberIsTooLarge(number1))return;
         number1 +=e.textContent;
         updateDisplay();
     }else if(isNumber1Set && !isNumber2Set){
+        if(checkIfNumberIsTooLarge(number2))return;
         number2 +=e.textContent;
         updateDisplay();
+    }
+}
+function handleEqualsButtonEvent(e){
+    if(isNumber1Set && isOperatorSet && number2!=""){
+        isNumber2Set = true;
+        operate(number1,number2,operator);
     }
 }
 function handleOperatorButtonsEvent(e){
     console.log("handleOperatorButtonsEvent function reached!")
     if(caluculateBasedOnResult){
-        number1 = result;
-        result = "";
-        caluculateBasedOnResult = false;
+        handleCalculateBasedOnResult();
     }
-    if(number1!=""){
+    if(isNumber1Set && isOperatorSet && number2!=""){
+        handleEqualsButtonEvent();
+        handleCalculateBasedOnResult();
+        operator = e.textContent;
+        isOperatorSet = true;
+        updateDisplay();
+    }
+    if(number1!="" && number2==""){
         operator = e.textContent;
         console.log(operator);
         isOperatorSet = true;
@@ -117,19 +146,19 @@ function handleOperatorButtonsEvent(e){
         updateDisplay();
     }
 }
+function handleClearButtonEvent(e){
+    savedResult ="";
+    caluculateBasedOnResult=false;
+    clear();
+}
 numberButtons.forEach(button => {
     button.addEventListener("click", (event) => handleNumberButtonsEvent(event.target));
 });
 operatorButtons.forEach(button =>{
     button.addEventListener("click", (event)=> handleOperatorButtonsEvent(event.target));     
 });
-equalsButton.addEventListener("click",(event)=>{
-    if(isNumber1Set && isOperatorSet && number2!=""){
-        isNumber2Set = true;
-        operate(number1,number2,operator);
-    }
-})
-clearButton.addEventListener("click",clear);
+equalsButton.addEventListener("click",(event)=>handleEqualsButtonEvent(event.target)); 
+clearButton.addEventListener("click", (event)=>handleClearButtonEvent(event.target));
 decimalButton.addEventListener("click", (event) => {
     if(caluculateBasedOnResult){
         handleCalculateBasedOnResult();
